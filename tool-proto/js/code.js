abbca56d32d9a1832c57820345d84c7edeabe2e6
Saad <saadanjum91@gmail.com>
2017-09-19 17:44:58 +0500

    table view basic

--- tool-proto/js/code.js
+++ tool-proto/js/code.js
@@ -1,5 +1,6 @@
 
-function openCreateTableModal(){
+
+var openCreateTableModal = function(){
 	$('#createTableColumns').html("");
 	$('#tablename-input').val("");
 	$('#createTableModal').modal('show');
@@ -9,6 +10,46 @@ function openCreateTableModal(){
 	storeHandler.store("tableId", tid+1, true);
 }
 
+function closeCreateTableModal(){
+	$('#createTableModal').modal('hide');
+}
+
+function refreshTablesView(){
+	$('#table-view-container').html("");
+	let currentTables = storeHandler.retrieve('tables');
+	currentTables.forEach(function(table){
+		let newHtml = `<div class="column">
+						<div class="ui segment">
+							<h3><a><i class="icon edit right floated"></i></a><span>${table.name}</span></h3>
+							<table class="ui table">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>Type</th>
+										<th>Keys</th>
+										<th>References</th>
+										<th>Modifiers</th>
+									</tr>
+								</thead>
+								<tbody>`;
+		for (colId in table.columns){
+			newHtml += `<tr>
+							<td>${table.columns[colId].title}</td><td>${table.columns[colId].type}</td>`;
+			newHtml += `<td>`;
+			newHtml += (table.columns[colId].is_primary_key) ? '[PK]' : '';
+			newHtml += (table.columns[colId].is_foreign_key) ? '[FK]' : '';
+			newHtml += '</td>';
+
+			newHtml += '<td>References</td>';
+			newHtml += `<td>${(table.columns[colId].auto_increment) ? '[AI]' : ''}</td>`;
+			newHtml += '</tr>';
+		}
+									
+		newHtml += `</tbody></table></div></div>`;
+		$('#table-view-container').append(newHtml);
+	});
+	
+}
 
 function makeNewColumnHTML(attributeId){
 	let columnid = `attr-${attributeId}`
@@ -17,7 +58,18 @@ function makeNewColumnHTML(attributeId){
 	return html
 }
 
-function addTableColumn(){
+
+var createTableCommit = function(){
+	let current = storeHandler.retrieve("newTable");
+	let freshCopy = Object.assign(current);
+	let currentTables = storeHandler.retrieve("tables");
+	currentTables.push(freshCopy);
+	closeCreateTableModal();
+	refreshTablesView();
+}
+
+
+var addTableColumn = function(){
 	let colId = storeHandler.retrieve("attrId");
 	$('#createTableColumns').append(makeNewColumnHTML(colId));
 	$('.ui.checkbox').checkbox();
@@ -71,6 +123,10 @@ function addTableColumn(){
 		current = storeHandler.retrieve("newTable");
 		current.columns[attrId].auto_increment = $(`#${fieldId}`).prop('checked');
 	});
+
+	if (Object.keys(currentTableState.columns).length > 0){
+		$('#createTableCommitBtn').attr('class', 'ui teal button');
+	}
 }
 
 
@@ -80,17 +136,38 @@ function removeColumn(e){
 	$(`#${colId}`).remove();
 	current = storeHandler.retrieve("newTable").columns;
 	delete current[colId];
+
+	console.log(Object.keys(current).length);
+	if (Object.keys(current).length > 0){
+		$('#createTableCommitBtn').attr('class', 'ui teal button');
+	} else {
+		$('#createTableCommitBtn').attr('class', 'ui teal button disabled');
+	}
 }
 
 $(document).ready(function(){
 	// init attribute and table id
 	storeHandler.store("attrId", 1);
 	storeHandler.store("tableId", 1);
+	storeHandler.store("tables", []);
 
 	// update table name in state
 	$('#tablename-input').change(function(){
 		let current = storeHandler.retrieve("newTable");
 		current.name = $('#tablename-input').val();
 	});
+
+	//bind buttons
+	$('#launcher-create-table').click(openCreateTableModal);
+	$('#addTableColumnBtn').click(addTableColumn);
+	$('#createTableCommitBtn').click(createTableCommit);
+
+	//vertical center menu
+	window.onresize= function(){
+		document.getElementById('side-menu').style.top = window.innerHeight/2 - (document.getElementById('side-menu').clientHeight+14)/2 + "px"
+	}
+	window.onload = function(){
+		document.getElementById('side-menu').style.top = window.innerHeight/2 - (document.getElementById('side-menu').clientHeight+14)/2 + "px"
+	}
 	
 });
